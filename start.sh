a8ec30553fedd009429d5fce3e8645384310dec3
Memes_land <58827868+Memesland-code@users.noreply.github.com>
2023-06-09 11:25:55 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Connecte le serveur avec le client, importe le fichier partagé, lance le serveur web
 smbclient -L 192.168.1.1 -Uclient%pi
 sudo PASSWD='pi' mount -t cifs //192.168.1.1/pishare /home/pi/server/static/photos -o username=client,dir_mode=0777,file_mode=0777
 . server/bin/activate
